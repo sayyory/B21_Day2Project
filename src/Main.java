96ca20abb6b43f2580f5a8cb8e6805530ce5737b
sayyory <sayyory@gmail.com>
2020-11-12 09:45:55 -0600

    B21 is dedicated

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last day");
         // B21 is exited !!!
+        //B21 is dedicated !!!
+
 
     }
 }
